6bbc8170e8778e783c9cbcd70dbe3afe856cb45c
adult-soul <39634836+adult-soul@users.noreply.github.com>
2018-05-26 12:43:43 -0700

    initial commit of index.html

---
+++ owl_002.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+		$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin:20,
+		autoplay:true,
+		autoWidth:true,
+		autoplayTimeout:2000,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:3,
+			},
+			600:{
+				items:5,
+			},
+			1000:{
+				items:8,
+			}
+		}
+		})
+	});
